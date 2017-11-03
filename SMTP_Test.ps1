eac3cd5f44519bd780a1d830bf3c8f420395e180
David <lachine2008@gmail.com>
2017-11-03 14:49:13 -0700

    Revert "update email address"
    This reverts commit 89c0ae1164ca0361d12c73b70c01a027d1d6dec1.

--- SMTP_Test.ps1
+++ SMTP_Test.ps1
@@ -1,1 +1,2 @@
-﻿Send-MailMessage -SMTPServer domain@domain.com -To domain@domain.com -From domain@domain.com -Subject "This is a test email" -Body "Hi all, this is a test email sent via PowerShell"
+﻿Send-MailMessage -SMTPServer mail3.builddirect.com -To davidqian@builddirect.com -From error@builddirect.com -Subject "This is a test email" -Body "Hi all, this is a test email sent via PowerShell"
+
