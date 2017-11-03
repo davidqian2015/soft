982af4a10469cf5a8057e6dc167ec98d79275b7a
David Qian <lachine2008@gmail.com>
2017-11-03 14:35:29 -0700

    init setup

---
+++ SMTP_Test.ps1
@@ -0,0 +1,2 @@
+﻿Send-MailMessage -SMTPServer mail3.builddirect.com -To davidqian@builddirect.com -From error@builddirect.com -Subject "This is a test email" -Body "Hi all, this is a test email sent via PowerShell"
+
